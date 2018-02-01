8f048075169b87f643fe0c0646b52b6a9b37020e
Chris Houston <chrishouston1@u.boisestate.edu>
2018-01-31 22:15:34 -0700

    Added content to index page

--- public/index.php
+++ public/index.php
@@ -1,34 +1,53 @@
 <!DOCTYPE html>
 <html>
-    
-    
-    <head>
-        <meta charset="UTF-8">
-        <link href="css/style.css" type="text/css" rel="stylesheet">
-        <title>Paylocity Demo - Home</title>
-    </head>
 
-    
-    <div class="header">
-        <a href="index.php"><img class="logo" src="images/paylocity_logo.png" alt="Paylocity Coding Challenge Logo" title="Paylocity Home" /></a>
-        <ul class="navlinks">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="employees.php">Employees</a></li>
-                <li><a href="calculate.php">Calculate</a></li>
-        </ul>
-    </div>
-    
-    
-    <div class="main">
-        <div class="container">
-        
+
+<head>
+    <meta charset="UTF-8">
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <title>Paylocity Demo - Home</title>
+</head>
+
+
+<div class="header">
+    <a href="index.php"><img class="logo" src="images/paylocity_logo.png" alt="Paylocity Coding Challenge Logo" title="Paylocity Home" /></a>
+    <ul class="navlinks">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="employees.php">Employees</a></li>
+        <li><a href="calculate.php">Calculate</a></li>
+    </ul>
+</div>
+
+
+<div class="main">
+    <div class="container">
+        <img class="center-image" src="images/coding_challenge.png">
+        <h1 class="coding-challenge-title">Paylocity Coding Challenge</h1>
+        <div class="coding-challenge-content">
+            <p>This website was created as a web application for Paylocity. This application will demonstrate the calculation a company paying for their employees' benefits package.
+            </p>
+            <h4>Requirements:</h4>
+            <ul>
+                <li>The cost of benefits is $1000/ear for each employee.</li>
+                <li>Each dependent (children and possibly spouse) incurs a cost of $500/year.</li>
+                <li>Anyone whose name starts with 'A' gets a 10% discount, employee or dependent.</li>
+            </ul>
+            <h4>Assumptions:</h4>
+            <ul>
+                <li>All employees are paid $2000 per paycheck before deductions.</li>
+                <li>There are 26 paychecks in a year.</li>
+            </ul>
+            <p>To get started, add a few employees (or select from the existing ones), then go to the Calculate page. From there, the application will give you a preview of the costs for the benefits package for the employee and from the requirements above.
+            </p>
+            <h3>Get started!</h3>
         </div>
     </div>
+</div>
 
-    
-    <div class="footer">
-        <p>Coding challenge by Chris Houston for Paylocity</p>
-    </div>
-    
-    
+
+<div class="footer">
+    <p>Coding challenge by Chris Houston for Paylocity</p>
+</div>
+
+
 </html>
